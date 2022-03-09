e32e280243eae96d460056ecef30155d7096ebcc
pwq <wq.peng@qq.com>
2022-03-09 15:57:07 +0000

    main(IE): add Iterative Elimination(IE) method

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import target
 def RIP(old, new):
     return (new - old) / old
 
+# Batch Elimination
 def BE():
     # get baseline execution time
     separator = ' '
@@ -31,9 +32,47 @@ def BE():
     result = [opts[index] for index in range(n_opt) if negative_indices[index] == 0]
     print("optimization choosen with BE method is :\n", result, "\n")
 
+# Iterative Elimination
+def IE():
+    # get baseline execution time
+    separator = ' '
+    flags = separator.join(opts)
+    target.compile(flags)
+    target.exec()
+    baseline_time = target.time()
+    target.record("T_baseline", baseline_time)
+
+    B = [1] * n_opt
+
+    while True:
+        # list of indexes of optimizations which are still enabled
+        S = [index for index in range(len(B)) if B[index] == 1]
+        times = []
+        # turn each off to find effect of each optimization
+        for off in S:
+            local_opts = [opts[i] for i in S if i != off]
+
+            flags = separator.join(local_opts)
+            target.compile(flags)
+            target.exec()
+            times.append(target.time())
+
+        most_negative = min(times)
+        target.record("most_neg_"+str(S[times.index(most_negative)]), most_negative)
+        # if all in S have non-negative effects, finish
+        # otherwise remove the most negative effect optimization
+        if most_negative >= baseline_time:
+            break
+        else:
+            B[S[times.index(most_negative)]] = 0
+
+    # select optimizations which are still enable
+    result = [opts[index] for index in range(n_opt) if B[index] == 1]
+    print("optimization choosen with IE method is :\n", result, "\n")
+
 
 opts = target.get_opts()
 n_opt = len(opts)
 print("Avail optimizations: \n", opts, "\n")
 
-BE()
+IE()
